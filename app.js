57cca57b04a46d938083ad4d6741c0092b7a1dce
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-01-10 09:36:01 +0530

    changes added to rashi features backend

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
 const rashiRoutes = require('./rashiRoutes');
+const cors = require('cors');
 
 const app = express();
 
 // 1. Body parser
 app.use(express.json({ limit: '10kb' }));
+
+// 2. Applying CORS
+app.use(cors());
 
 // 2. Serving static files: All static assets will be served through the Public folder
 app.use(express.static(path.join(__dirname, 'public')));
